36d5a185c57a20423c4c792a87ff52912e9e87b3
Renato Böhler <renato.bohler@gmail.com>
2018-09-08 15:27:17 -0300

    implementa penalidade

---
+++ GeneticAlgorithm/src/Penalty.py
@@ -0,0 +1,75 @@
+# Autores: Davi Boberg e Renato Böhler
+
+# Penalidade aplicada
+penaltyAmount = 1000
+
+# "Granularidade" de cada quadrado da cena
+divisonsPerUnit = 100
+
+# Limites da parede da cena
+YL1 = 4.5 * divisonsPerUnit
+YL2 = 5.5 * divisonsPerUnit
+XL3 = 3.5 * divisonsPerUnit
+
+# Cálculo da penalidade ao sair de origem ir a destino
+def penalty(origem, destino):
+    c_origem = _convertStateToCoordinates(origem)
+    c_destino = _convertStateToCoordinates(destino)
+
+    x = c_origem[0]
+    y = c_origem[1]
+
+    x0 = c_destino[0]
+    y0 = c_destino[1]
+    
+    print("Origem: ({x},{y})".format(x=x,y=y))
+    print("Destino: ({x},{y})".format(x=x0,y=y0))
+
+    # Mesmo ponto
+    if (x == x0 and y == y0):
+        return 0
+    
+    # Coeficiente angular infinito
+    if (x == x0):
+        if (x >= XL3):
+            return penaltyAmount
+        return 0
+
+    # Coeficiente angular nulo
+    if (y == y0):
+        if (YL1 <= y and y <= YL2):
+            return penaltyAmount
+        return 0
+
+    # Demais casos
+    # Coeficiente angular
+    a = (y - y0) / (x - x0)
+    print("a = {a}".format(a=a))
+
+    # Coeficiente linear
+    b = y - a * x
+    print("b = {b}".format(b=b))
+
+    # Limite 1
+    XL1 = (YL1 - b) / a    
+    if XL1 >= XL3:
+        print("XL1 = {XL1} >= {XL3} = XL3".format(XL1=XL1,XL3=XL3))
+        return penaltyAmount
+
+    # Limite 2
+    XL2 = (YL2 - b) / a
+    if XL2 >= XL3:
+        print("XL2 = {XL2} >= {XL3} = XL3".format(XL2=XL2,XL3=XL3))
+        return penaltyAmount
+
+    # Limite 3
+    YL3 = a * XL3 + b
+    if YL1 <= YL3 and YL3 <= YL2:
+        print("YL1 = {YL1} <= {YL3} <= {YL2} = YL2".format(YL1=YL1,YL2=YL2,YL3=YL3))
+        return penaltyAmount
+    
+    return 0
+
+# Converte um estado (x, y, D) para suas respectivas coordenadas
+def _convertStateToCoordinates(state):
+    return (state[0] * divisonsPerUnit,state[1] * divisonsPerUnit)
